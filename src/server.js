65f3e301a31a266f3c8b9699e0df50357e4bd862
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2019-07-22 16:39:32 -0400

    Created custom routers

--- src/server.js
+++ src/server.js
@@ -12,4 +12,22 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+//Custom middleware
+const log = (req, res, next) => {
+  console.log('logging')
+  req.mydata = 'holitas'
+  //next(new Error('yes')) // If you send a parameter to next function it throws an error it should
+  next()
+}
+// In this way the middleware is executed as global
+//app.use(log);
+// In this way the middleware is executed only for this endpoint
+app.get('/', [log, log], (req, res) => {
+  res.send({ data: req.mydata })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server is running on 3000')
+  })
+}
